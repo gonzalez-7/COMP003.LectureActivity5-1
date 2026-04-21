251c363e74e27ce081121a0f24c9f7e687de08ab
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-20 21:27:55 -0700

    updated the program,cs

--- COMP003.LectureActivity5/Program.cs
+++ COMP003.LectureActivity5/Program.cs
@@ -6,14 +6,12 @@ namespace COMP003.LectureActivity5
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
 			builder.Services.AddControllers();
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
@@ -23,12 +21,6 @@ namespace COMP003.LectureActivity5
 			app.UseHttpsRedirection();
 			app.UseAuthorization();
 			app.MapControllers();
-
-			app.MapStaticAssets();
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}")
-				.WithStaticAssets();
 
 			app.Run();
 		}
